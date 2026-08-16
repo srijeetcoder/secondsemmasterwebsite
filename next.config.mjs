c00b1f96be1ac901d842a4afe682fed6a04eff3d
Srijeet Chatterjee <143578280+srijeetcoder@users.noreply.github.com>
2026-08-16 12:50:38 +0530

    Update next.config.mjs for export output and images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
